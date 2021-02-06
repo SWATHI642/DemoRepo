4cb61ee5d1700d0af94daae27d46a0650198356c
swathi <mswathikc@gmail.com>
2021-02-06 18:37:58 +0000

    new file added

---
+++ demo2.java
@@ -0,0 +1,10 @@
+class demo2
+{
+	public static void main(String args[])
+	{
+	 System.out.println("line 1 of demo5");
+	
+	 System.out.println("line 2 of demo5");
+  System.out.println("line 3 of demo5");
+	}
+}
---
+++ demo3.java
@@ -0,0 +1,15 @@
+class demo3
+{
+	public static void main(String args[])
+	{
+	 System.out.println("line 1 of demo5");
+	
+	 System.out.println("line 2 of demo5");
+  System.out.println("line 3 of demo5");
+   System.out.println("line 4 of demo5");
+	}
+
+	
+
+}
+
