e0866d8af9b9d55ed9fe8c57a117972136b2f50b
SWATHI642 <78651885+SWATHI642@users.noreply.github.com>
2021-02-07 01:46:41 +0530

    Updated demo1 from remote repo

--- demo1.java
+++ demo1.java
@@ -3,8 +3,8 @@ class demo5
 	public static void main(String args[])
 	{
 	 System.out.println("line 1 of demo5");
-	
 	 System.out.println("line 2 of demo5");
+	 System.out.println("line 3 of demo5 from remote repo ");
 	}
 
 }
